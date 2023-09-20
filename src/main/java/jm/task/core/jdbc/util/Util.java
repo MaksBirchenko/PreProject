d2dabeaf77410085f597f48bf94d66dd34935b01
Maksim Birchenko <max.birchenko@mail.ru>
2023-09-21 01:04:56 +0300

    Change util,change main,add depend. to pom.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,21 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String password = "1234";
+    private static final String username = "postgres";
+    private static final String url = "jdbc:postgresql://localhost:5432/maksbirchenko";
+    private Util(){
+    }
+    public static Connection open(){
+        try {
+            return DriverManager.getConnection(url,username,password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
